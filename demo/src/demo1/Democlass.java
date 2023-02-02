5f0f9deb9dc7da457d233269be957e60e3b9f963
Roshni katre <roshnisharnagat96@gmail.com>
2023-02-02 17:03:00 +0530

    basic task

---
+++ demo/src/demo1/Democlass.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class Democlass {
+	public static void main(String[] args) {
+		System.out.println("testbase");
+		System.out.println("pom.xml");
+		System.out.println("utilities");
+
+	}
+
+}
